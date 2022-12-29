943f0be80efb849671ae6e91fbb86f830ee1007d
Kubrianity <kubraaozerenn@gmail.com>
2022-12-29 18:49:29 +0300

    Center button in center of the page with bootstrap class

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,8 @@
 function Button({handleClick}) {
   return (
-    <button onClick = {handleClick} type = "button" className = "btn btn-light">Start</button>
+    <div className = "position-absolute top-50 start-50 translate-middle" >
+      <button onClick = {handleClick} type = "button" className = "btn btn-warning">Start</button>
+    </div>
   )
 }
 export default Button
